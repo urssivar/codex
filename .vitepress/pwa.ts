fea5a5747817a4f4f86d7b7d7013409f5ee81bcd
alkaitagi <alkaitagi@outlook.com>
2023-04-28 00:11:18 +0300

    Fix PWA

---
+++ .vitepress/pwa.ts
@@ -0,0 +1,16 @@
+export const pwa = {
+    base: '/urssivar/',
+    registerType: 'autoUpdate',
+    manifest: {
+        name: 'Urssivar',
+        short_name: 'Urssivar Kaitag',
+        theme_color: '#ffffff',
+        icons: [
+            {
+                src: 'favicon.png',
+                sizes: '144x144',
+                type: 'image/png',
+            },
+        ],
+    },
+};
