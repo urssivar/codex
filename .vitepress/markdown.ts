353d4fa496f72d39d6f3817acc0f6378dcf98e74
alkaitagi <alkaitagi@outlook.com>
2023-05-16 08:02:16 +0300

    Join hint & audio samples

--- .vitepress/markdown.ts
+++ .vitepress/markdown.ts
@@ -1,12 +1,20 @@
 import MarkdownIt from "markdown-it";
 
 export default function configureMarkdown(md: MarkdownIt) {
-  hintSample(md);
-  audioSample(md);
+  parseSample(md);
   phraseTable(md);
 
   md.use(require("markdown-it-attrs"));
   md.use(require("markdown-it-bracketed-spans"));
+}
+
+function rend(s: string, md: MarkdownIt) {
+  s = md.render(s);
+  const pf = "<p>";
+  if (s.startsWith(pf)) s = s.substring(pf.length);
+  const sf = "</p>\n";
+  if (s.endsWith(sf)) s = s.substring(0, s.length - sf.length);
+  return s;
 }
 
 function phraseTable(md: MarkdownIt) {
@@ -47,40 +55,29 @@ function phraseTable(md: MarkdownIt) {
   });
 }
 
-function hintSample(md: MarkdownIt) {
+function parseSample(md: MarkdownIt) {
   const mreg = require("markdown-it-regexp");
   md.use(
-    mreg(/\#\[(.+?)\|(.+?)\]/, (match) => {
-      const [, c, h] = match;
-      return (
-        `<Word h="${h}">` +
-        rend(c, md) +
-        "<template #content>" +
-        rend(h, md) +
-        "</template>" +
-        "</Word>"
-      );
+    mreg(/\#\[(.+?)\](\(.+?\))?/, (match) => {
+      const [, content, url] = match;
+      let html = "";
+      if (content.includes("|")) {
+        const [c, ...h] = content.split("|");
+        html =
+          `<Word>` +
+          rend(c, md) +
+          "<template #content>" +
+          rend(h.join("|"), md) +
+          "</template>" +
+          "</Word>";
+      } else html = rend(content, md);
+      if (url) {
+        const u = url.substring(1, url.length - 1);
+        html = `<Say url="${u}">${html}</Say>`;
+      }
+      return html;
     })
   );
-}
-
-function audioSample(md: MarkdownIt) {
-  const mreg = require("markdown-it-regexp");
-  md.use(
-    mreg(/\^\[(.+)\]\((.+)\)/, (match) => {
-      const [, c, u] = match;
-      return `<Say url="${u}">${rend(c, md)}</Say>`;
-    })
-  );
-}
-
-function rend(s: string, md: MarkdownIt) {
-  s = md.render(s);
-  const pf = "<p>";
-  if (s.startsWith(pf)) s = s.substring(pf.length);
-  const sf = "</p>\n";
-  if (s.endsWith(sf)) s = s.substring(0, s.length - sf.length);
-  return s;
 }
 
 function parseTable(md: MarkdownIt, tokens: any[]) {
