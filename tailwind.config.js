2155dd0de3d9616ccc09288f3eb92df3322dda08
alkaitagi <alkaitagi@outlook.com>
2024-04-29 19:57:54 +0300

    Install TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./docs/**/*.md",
+    "./components/**/*.{vue,js,ts,jsx,tsx}",
+    "./.vitepress/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
