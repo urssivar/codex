35c8b16c596c7667a8c94197e7fc89a6efc5083f
alkaitagi <alkaitagi@outlook.com>
2025-04-25 00:15:20 +0300

    Improve word styling

--- components/Dictionary/DWord.vue
+++ components/Dictionary/DWord.vue
@@ -12,20 +12,20 @@ defineProps<{
 </script>
 
 <template>
-    <div class="root tw-relative tw-break-inside-avoid tw-rounded-md tw-flex tw-flex-col tw-gap-0.5 tw-py-2">
+    <div class="root tw-relative tw-break-inside-avoid tw-rounded-md tw-flex tw-flex-col tw-gap-0.5 tw-py-1">
         <div class="tw-flex tw-items-center tw-justify-between">
             <h6 :id="word.id">{{ word.headword }}</h6>
             <span class="ipa tw-text-sm">{{ word.ipa }}</span>
         </div>
-        <div class="tw-flex tw-items-center tw-gap-1">
-            <span class="tag tw-leading-4 tw-text-xs tw-font-semibold tw-rounded tw-px-1 tw-py-px"
+        <div class="tw-flex tw-text-xs tw-items-center tw-gap-1">
+            <span class="tag tw-leading-4 tw-font-semibold tw-rounded tw-px-1 tw-py-px"
                 v-for="t in word.tags">
                 {{ t }}
             </span>
-            <span class="tw-text-sm tw-italic" v-for="(f, i) in word.forms">
+            <span class="tw-italic" v-for="(f, i) in word.forms">
                 {{ f + (i < word.forms.length - 1 ? ',' : '') }} </span>
         </div>
-        <div>
+        <div class="tw-ml-4">
             <li class="!tw-m-0" v-for="d in word.definitions">
                 {{ d }}
             </li>
