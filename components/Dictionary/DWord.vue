724fcb35af36898230493ed264556113ad887bd6
alkaitagi <alkaitagi@outlook.com>
2025-04-24 23:48:42 +0300

    Improve word style

--- components/Dictionary/DWord.vue
+++ components/Dictionary/DWord.vue
@@ -12,19 +12,21 @@ defineProps<{
 </script>
 
 <template>
-    <div class="root">
-        <div class="title">
+    <div class="root tw-relative tw-break-inside-avoid tw-rounded-md tw-flex tw-flex-col tw-gap-0.5 tw-py-2">
+        <div class="tw-flex tw-items-center tw-justify-between">
             <h6 :id="word.id">{{ word.headword }}</h6>
-            <span class="ipa">{{ word.ipa }}</span>
+            <span class="ipa tw-text-sm">{{ word.ipa }}</span>
         </div>
-        <div class="subtitle">
-            <span class="tag" v-for="t in word.tags">
+        <div class="tw-flex tw-items-center tw-gap-1">
+            <span class="tag tw-leading-4 tw-text-xs tw-font-semibold tw-rounded tw-px-1 tw-py-px"
+                v-for="t in word.tags">
                 {{ t }}
             </span>
-            {{ word.forms.join(', ') }}
+            <span class="tw-text-sm tw-italic" v-for="(f, i) in word.forms">
+                {{ f + (i < word.forms.length - 1 ? ',' : '') }} </span>
         </div>
         <div>
-            <li v-for="d in word.definitions">
+            <li class="!tw-m-0" v-for="d in word.definitions">
                 {{ d }}
             </li>
         </div>
@@ -34,14 +36,6 @@ defineProps<{
 <style scoped>
 .root {
     transition: 150ms;
-
-    position: relative;
-    break-inside: avoid;
-    border-radius: 8px;
-    padding: 8px 0;
-    display: flex;
-    flex-direction: column;
-    gap: 6px;
 
     &::before {
         content: '';
@@ -58,45 +52,14 @@ defineProps<{
     &:hover::before {
         background-color: var(--vp-code-block-bg);
     }
-
-    .title {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-
-        >.ipa {
-            margin: 0;
-
-        }
-    }
-
-    .subtitle {
-        display: flex;
-        gap: 4px;
-        font-size: small;
-        font-style: italic;
-        color: var(--vp-c-text-2);
-    }
-
-    li {
-        font-size: 14px;
-        margin: 0 !important;
-    }
 }
 
 .ipa {
-    font-size: small;
     color: var(--vp-c-text-2);
-    font-weight: normal;
     font-family: "Noto Sans", sans-serif;
 }
 
 .tag {
     background-color: var(--vp-c-default-soft);
-    font-weight: bolder;
-    font-size: 11px;
-    border-radius: 4px;
-    font-style: normal;
-    padding: 2px 4px;
 }
 </style>
