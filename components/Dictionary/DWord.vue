6037104217f1d731ba97b7b80e698a0c75226bf4
alkaitagi <alkaitagi@outlook.com>
2025-04-26 23:02:27 +0300

    Remove ipa in word card

--- components/Dictionary/DWord.vue
+++ components/Dictionary/DWord.vue
@@ -13,19 +13,19 @@ defineProps<{
 
 <template>
     <div class="root tw-relative tw-break-inside-avoid tw-rounded-md tw-flex tw-flex-col tw-gap-0.5 tw-py-1">
-        <div class="tw-flex tw-items-center tw-justify-between">
-            <h6 :id="word.id" class=" tw-tracking-wide">{{ word.headword }}</h6>
-            <span class="ipa tw-text-sm">{{ word.ipa }}</span>
-        </div>
-        <div class="tw-flex tw-text-xs tw-items-center tw-gap-1">
-            <span class="tag tw-leading-4 tw-font-semibold tw-rounded tw-px-1 tw-py-px"
+        <div class="tw-flex tw-items-center tw-gap-1">
+            <h6 :id="word.id" class="tw-tracking-[0.035em]">
+                {{ word.headword }}
+            </h6>
+            <span class="tag tw-text-xs tw-leading-4 tw-font-semibold tw-rounded tw-px-1 tw-py-px"
                 v-for="t in word.tags">
                 {{ t }}
             </span>
-            <span class="tw-italic" v-for="(f, i) in word.forms">
-                {{ f + (i < word.forms.length - 1 ? ',' : '') }} </span>
         </div>
-        <div class="tw-ml-4">
+        <span class="tw-italic tw-text-xs">
+            {{ word.forms.join(', ') }}
+        </span>
+        <div class="tw-ml-4 tw-text-sm">
             <li class="!tw-m-0" v-for="d in word.definitions">
                 {{ d }}
             </li>
@@ -52,11 +52,6 @@ defineProps<{
     &:hover::before {
         background-color: var(--vp-code-block-bg);
     }
-}
-
-.ipa {
-    color: var(--vp-c-text-2);
-    font-family: "Noto Sans", sans-serif;
 }
 
 .tag {
