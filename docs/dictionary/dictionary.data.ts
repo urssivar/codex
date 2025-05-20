fe7039419dfcbba39f326c5d55fc7979f526e715
alkaitagi <alkaitagi@outlook.com>
2025-05-20 21:10:35 +0300

    Fix letters routing

--- docs/dictionary/dictionary.data.ts
+++ docs/dictionary/dictionary.data.ts
@@ -3,13 +3,5 @@ import dictionary from "./dictionary.json";
 export default {
     load() {
         return dictionary;
-        // const letters = ['а', 'пь', 'ҳ'];
-        // return letters.reduce(
-        //     (dict, letter) => {
-        //         dict[letter] = dictionary[letter];
-        //         return dict;
-        //     },
-        //     {}
-        // );
     }
 }
